e482a3bbbafd87431fbb01ad75828dc57ad27ff2
arturop <67555134+arturopstem@users.noreply.github.com>
2024-03-19 14:47:40 -0500

    Add hash method

--- src/hash-map.js
+++ src/hash-map.js
@@ -4,6 +4,19 @@ class HashMap {
   constructor(numberOfBuckets = 16) {
     this.#buckets = Array(numberOfBuckets).fill(null);
   }
+
+  #hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    const numberOfBuckets = this.#buckets.length;
+
+    for (let i = 0; i < key.length; i += 1) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode %= numberOfBuckets;
+    }
+
+    return hashCode;
+  }
 }
 
 export default HashMap;
